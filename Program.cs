b2cfeaf31d196e47bb97f53f70464a34d400d594
Mônica <monicambs@1@gmail.com>
2022-09-11 14:10:16 -0300

    resolução

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Praia grande");
+        Console.ReadKey();
+        Console.WriteLine("Mongaguá");
+        Console.ReadKey();
+        Console.WriteLine("Itanhaém");
+        Console.ReadKey();
+        Console.WriteLine("Peruíbe");
+        Console.ReadKey();
+    }
+}
